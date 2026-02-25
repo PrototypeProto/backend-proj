2f828ba91ccf1b41a6446b10353e45dcbb6d0d4b
prototypeProto <anunfoundx@gmail.com>
2026-02-24 21:24:08 -0500

    removed local enums & imported centralized enums

--- src/auth/dependencies_data.py
+++ src/auth/dependencies_data.py
@@ -2,29 +2,13 @@ from .dependencies import RoleChecker
 from fastapi.exceptions import HTTPException
 from fastapi import Depends
 from enum import Enum
-
-class MemberRole(str, Enum):
-    '''
-        May deprecate ADMIN role in favor of president+VP role, TBD
-    '''
-    ADMIN = "admin"
-    COACH = "coach"
-    OFFICER = "officer"
-    MEMBER = "member"
-    INACTIVE = "inactive"
-
-class Semester(str, Enum):
-    '''
-        NO practice during the summer, but placeholder in case situation changes
-    '''
-    SPRING = "spring"
-    SUMMER = "summer"
-    FALL = "fall"
+from src.db.db_enum_models import MemberRoleEnum
 
 
 role_checker = Depends(RoleChecker(['member']))
-admin_rolechecker = Depends(RoleChecker([MemberRole.ADMIN]))
-coach_rolechecker = Depends(RoleChecker([MemberRole.ADMIN, MemberRole.COACH]))
-officer_rolechecker = Depends(RoleChecker([MemberRole.ADMIN, MemberRole.OFFICER]))
-member_rolechecker = Depends(RoleChecker([MemberRole.ADMIN, MemberRole.COACH, MemberRole.OFFICER]))
-public_rolechecker = Depends(RoleChecker([role for role in MemberRole]))
+admin_rolechecker = Depends(RoleChecker([MemberRoleEnum.ADMIN]))
+coach_rolechecker = Depends(RoleChecker([MemberRoleEnum.ADMIN, MemberRoleEnum.COACH]))
+officer_rolechecker = Depends(RoleChecker([MemberRoleEnum.ADMIN, MemberRoleEnum.OFFICER]))
+member_rolechecker = Depends(RoleChecker([MemberRoleEnum.ADMIN, MemberRoleEnum.COACH, MemberRoleEnum.OFFICER, MemberRoleEnum.MEMBER]))
+general_member_rolechecker = Depends(RoleChecker([MemberRoleEnum.ADMIN, MemberRoleEnum.COACH, MemberRoleEnum.OFFICER, MemberRoleEnum.MEMBER, MemberRoleEnum.INACTIVE, MemberRoleEnum.ALUMNI]))
+public_rolechecker = Depends(RoleChecker([role for role in MemberRoleEnum]))
